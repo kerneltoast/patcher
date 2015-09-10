8e2b28432dd7eabaf6ece4637e9a09f0b0fd3fc4
Sultanxda <sultanxda@gmail.com>
2015-09-09 19:46:13 -0700

    patcher: Add new patches
    *Added strict-mode patch
    *Added updated liblog patch
    *Added patches from nightly branch not-yet merged into the stable branch

--- patcher.sh
+++ patcher.sh
@@ -13,10 +13,18 @@ git clean -f -d
 cd ../../frameworks/base
 git reset --hard
 wget https://github.com/CyanogenMod/android_frameworks_base/commit/b68ae542f9999a6314e3bc6a13db800e18d14fe5.patch && patch -p1 < b68ae542f9999a6314e3bc6a13db800e18d14fe5.patch
+wget https://github.com/CyanogenMod/android_frameworks_base/commit/cc9ef392e40226cab2ee89a87e8af4ef4b1ba979.patch && patch -p1 < cc9ef392e40226cab2ee89a87e8af4ef4b1ba979.patch
+wget https://github.com/CyanogenMod/android_frameworks_base/commit/e75f59e7fd349dd1fa5d452086c795f693776d89.patch && patch -p1 < e75f59e7fd349dd1fa5d452086c795f693776d89.patch
+git clean -f -d
+
+# packages/apps/Settings:
+cd ../../packages/apps/Settings
+git reset --hard
+wget https://github.com/sultanxda/android_packages_apps_Trebuchet/commit/970f063806b488618968525eafb80f2b69d13ef3.patch && patch -p1 < 970f063806b488618968525eafb80f2b69d13ef3.patch
 git clean -f -d
 
 # packages/apps/Trebuchet:
-cd ../../packages/apps/Trebuchet
+cd ../../../packages/apps/Trebuchet
 git reset --hard
 wget https://github.com/sultanxda/android_packages_apps_Trebuchet/commit/cb8b36c7d9f0cff549b0322eabedea6820d9b52c.patch && patch -p1 < cb8b36c7d9f0cff549b0322eabedea6820d9b52c.patch
 git clean -f -d
@@ -24,5 +32,6 @@ git clean -f -d
 # system/core:
 cd ../../../system/core
 git reset --hard
-wget https://github.com/sultanxda/android_system_core/commit/d19be9fd37f78524cae0cac302ce2b51f3fcfe5c.patch && patch -p1 < d19be9fd37f78524cae0cac302ce2b51f3fcfe5c.patch
+wget https://github.com/sultanxda/android_system_core/commit/c407c8a2299183ce0fd0e7f7b1c026a66b5adb8d.patch && patch -p1 < c407c8a2299183ce0fd0e7f7b1c026a66b5adb8d.patch
 git clean -f -d
+cd ../..
--- unpatcher.sh
+++ unpatcher.sh
@@ -12,8 +12,13 @@ cd ../../frameworks/base
 git reset --hard
 git clean -f -d
 
+# packages/apps/Settings:
+cd ../../packages/apps/Settings
+git reset --hard
+git clean -f -d
+
 # packages/apps/Trebuchet:
-cd ../../packages/apps/Trebuchet
+cd ../../../packages/apps/Trebuchet
 git reset --hard
 git clean -f -d
 
@@ -21,3 +26,4 @@ git clean -f -d
 cd ../../../system/core
 git reset --hard
 git clean -f -d
+cd ../..
