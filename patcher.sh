f36c5b5dc1564a7aa60612d0eb3f793cf1fbfb29
Sultanxda <sultanxda@gmail.com>
2015-11-12 19:53:59 -0800

    Add patch for SystemUI FC with hw-button long press

--- patcher.sh
+++ patcher.sh
@@ -20,6 +20,8 @@ wget https://github.com/CyanogenMod/android_frameworks_base/commit/c783d6643ecc6
 patch -p1 < c783d6643ecc607e917e27091bb4d6b126c470d4.patch
 wget https://github.com/sultanxda/android_frameworks_base/commit/1849d8f5b274a69bd3b11566005659e6f94ec25b.patch
 patch -p1 < 1849d8f5b274a69bd3b11566005659e6f94ec25b.patch
+wget https://github.com/CyanogenMod/android_frameworks_base/commit/3c0a9b2bc72d885ad0161c49c5881238157068d3.patch
+patch -p1 < 3c0a9b2bc72d885ad0161c49c5881238157068d3.patch
 git clean -f -d
 
 # packages/apps/Settings:
