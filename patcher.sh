8de28094c865e488c9406b049c041fbcca675836
Sultanxda <sultanxda@gmail.com>
2015-12-03 20:25:29 -0800

    Update 1440p patches

--- patcher.sh
+++ patcher.sh
@@ -5,15 +5,17 @@
 # frameworks/av:
 cd frameworks/av
 git reset --hard
-wget https://github.com/CyanogenMod/android_frameworks_av/commit/25497dcfac47704d53f1dba7fba16d24786aa7e5.patch
-patch -p1 < 25497dcfac47704d53f1dba7fba16d24786aa7e5.patch
+wget https://github.com/sultanxda/android_frameworks_av/commit/9bba479d69ed42b7a88df68974df49734e361a9b.patch
+patch -p1 < 9bba479d69ed42b7a88df68974df49734e361a9b.patch
+wget https://github.com/sultanxda/android_frameworks_av/commit/44b8ecca7a07b05c71e595c03750a9f5915bce35.patch
+patch -p1 < 44b8ecca7a07b05c71e595c03750a9f5915bce35.patch
 git clean -f -d
 
 # frameworks/base:
 cd ../../frameworks/base
 git reset --hard
-wget https://github.com/CyanogenMod/android_frameworks_base/commit/f53f47232674caf2a03482f8e7fe5be8e01adfa3.patch
-patch -p1 < f53f47232674caf2a03482f8e7fe5be8e01adfa3.patch
+wget https://github.com/sultanxda/android_frameworks_base/commit/2002fc9e3233171e2cd821728f3c98c945ce00c6.patch
+patch -p1 < 2002fc9e3233171e2cd821728f3c98c945ce00c6.patch
 wget https://github.com/CyanogenMod/android_frameworks_base/commit/c783d6643ecc607e917e27091bb4d6b126c470d4.patch
 patch -p1 < c783d6643ecc607e917e27091bb4d6b126c470d4.patch
 wget https://github.com/sultanxda/android_frameworks_base/commit/1849d8f5b274a69bd3b11566005659e6f94ec25b.patch
