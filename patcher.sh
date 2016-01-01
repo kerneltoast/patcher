06cee4d2c488f5446c370460c9757919e9462c77
Sultanxda <sultanxda@gmail.com>
2015-12-31 19:43:30 -0800

    Perform git clean prior to downloading patch

--- patcher.sh
+++ patcher.sh
@@ -4,14 +4,14 @@
 
 # frameworks/av:
 cd frameworks/av
-git reset --hard
+git reset --hard && git clean -f -d
 wget https://github.com/sultanxda/android_frameworks_av/commit/44b8ecca7a07b05c71e595c03750a9f5915bce35.patch
 patch -p1 < 44b8ecca7a07b05c71e595c03750a9f5915bce35.patch
 git clean -f -d
 
 # frameworks/base:
 cd ../../frameworks/base
-git reset --hard
+git reset --hard && git clean -f -d
 wget https://github.com/sultanxda/android_frameworks_base/commit/2002fc9e3233171e2cd821728f3c98c945ce00c6.patch
 patch -p1 < 2002fc9e3233171e2cd821728f3c98c945ce00c6.patch
 wget https://github.com/CyanogenMod/android_frameworks_base/commit/c783d6643ecc607e917e27091bb4d6b126c470d4.patch
@@ -24,21 +24,21 @@ git clean -f -d
 
 # packages/apps/Trebuchet:
 cd ../../packages/apps/Trebuchet
-git reset --hard
+git reset --hard && git clean -f -d
 wget https://github.com/sultanxda/android_packages_apps_Trebuchet/commit/5cbf273ce872ed51887e90cd54ef0283ac2987b6.patch
 patch -p1 < 5cbf273ce872ed51887e90cd54ef0283ac2987b6.patch
 git clean -f -d
 
 # system/core:
 cd ../../../system/core
-git reset --hard
+git reset --hard && git clean -f -d
 wget https://github.com/sultanxda/android_system_core/commit/a1702ced972dff3608d3808cfe61b524af887804.patch
 patch -p1 < a1702ced972dff3608d3808cfe61b524af887804.patch
 git clean -f -d
 
 # vendor/cm:
 cd ../../vendor/cm
-git reset --hard
+git reset --hard && git clean -f -d
 wget https://github.com/CyanogenMod/android_vendor_cm/commit/9d78806a29f49b58c1aeed93e530184b49eb56e5.patch
 patch -p1 < 9d78806a29f49b58c1aeed93e530184b49eb56e5.patch
 rm -f 9d78806a29f49b58c1aeed93e530184b49eb56e5.patch
