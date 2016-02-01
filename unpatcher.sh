3e963cb4181ece7b3eca1ea3e3609392b352856d
Sultanxda <sultanxda@gmail.com>
2016-02-01 15:21:03 -0800

    Remove patches irrelevant to V400

--- unpatcher.sh
+++ unpatcher.sh
@@ -2,28 +2,8 @@
 
 # Remove previously-applied patches
 
-# frameworks/av:
-cd frameworks/av
-git reset --hard
-git clean -f -d
-
 # frameworks/base:
-cd ../../frameworks/base
-git reset --hard
-git clean -f -d
-
-# frameworks/opt/telephony:
-cd ../../frameworks/opt/telephony
-git reset --hard
-git clean -f -d
-
-# packages/apps/Trebuchet:
-cd ../../../packages/apps/Trebuchet
-git reset --hard
-git clean -f -d
-
-# system/core:
-cd ../../../system/core
+cd frameworks/base
 git reset --hard
 git clean -f -d
 cd ../..
