55412377c42df9548e3855d2dec23b7dc022db3a
Sultanxda <sultanxda@gmail.com>
2016-04-03 04:05:35 -0700

    Remove recent upstream changes that break the lock screen

--- patcher.sh
+++ patcher.sh
@@ -19,6 +19,7 @@ git clean -f -d
 # frameworks/base:
 cd ../../frameworks/base
 git reset --hard && git clean -f -d
+git revert --no-edit 9a8df5bc8488c19dc0a1951cad44956894a75a09
 wget -q https://github.com/sultanxda/android_frameworks_base/commit/2002fc9e3233171e2cd821728f3c98c945ce00c6.patch
 patch -p1 -s < 2002fc9e3233171e2cd821728f3c98c945ce00c6.patch
 wget -q https://github.com/CyanogenMod/android_frameworks_base/commit/c783d6643ecc607e917e27091bb4d6b126c470d4.patch
@@ -41,8 +42,13 @@ wget -q https://github.com/sultanxda/android_packages_services_Telecomm/commit/f
 patch -p1 < f3af3472207d481d8636a17d8f49c3ebf5e814d6.patch
 git clean -f -d
 
+# prebuilts/cmsdk:
+cd ../../../prebuilts/cmsdk
+git reset --hard && git clean -f -d
+git revert --no-edit 154fdff15315ae83ac31ef359f41a36c9e7b8180
+
 # system/core:
-cd ../../../system/core
+cd ../../system/core
 git reset --hard && git clean -f -d
 wget -q https://github.com/sultanxda/android_system_core/commit/a1702ced972dff3608d3808cfe61b524af887804.patch
 patch -p1 -s < a1702ced972dff3608d3808cfe61b524af887804.patch
@@ -58,6 +64,7 @@ wget -q https://raw.githubusercontent.com/CyanogenMod/android_vendor_cm/cm-13.0/
 # vendor/cmsdk:
 cd ../../../../../vendor/cmsdk
 git reset --hard && git clean -f -d
+git revert --no-edit d549053b2df52f27e43c43c2dca939b5428c322e
 wget -q https://github.com/sultanxda/cm_platform_sdk/commit/2d9efe7427cf3d8a8134a5652192b5266a7335d1.patch
 patch -p1 -s < 2d9efe7427cf3d8a8134a5652192b5266a7335d1.patch
 git clean -f -d
