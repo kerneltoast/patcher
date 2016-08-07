964d0993b655a7bb816b95d9a60b60773258bec0
Sultanxda <sultanxda@gmail.com>
2016-08-07 12:46:40 -0700

    Revert "Add upstream Bluetooth stutter fix"
    No-op for us. Bacon does not use a Broadcom chipset.
    
    This reverts commit 17abef7a0f94e32f9a7f1d123a82901442182055.

--- unpatcher.sh
+++ unpatcher.sh
@@ -21,8 +21,6 @@ cd $ROM_TREE/packages/apps/Settings
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/services/Telephony
 git clean -f -d && git reset --hard
-cd $ROM_TREE/system/bt
-git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
 cd $ROM_TREE/vendor/cm
