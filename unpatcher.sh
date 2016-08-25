df0c70bfc18e20037915b91c55effc3e97339591
Sultanxda <sultanxda@gmail.com>
2016-08-25 15:24:35 -0700

    Revert media button changes to Eleven
    Eleven is randomly playing music while there is no headset plugged in.

--- unpatcher.sh
+++ unpatcher.sh
@@ -9,6 +9,8 @@ cd $ROM_TREE/frameworks/av
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Eleven
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Messaging
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Nfc
