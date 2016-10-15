e567b3bfaadf53698edd2b5ab46e3f39ab568d11
Sultanxda <sultanxda@gmail.com>
2016-10-15 15:56:48 -0700

    Add October 1, 2016 security patches

--- unpatcher.sh
+++ unpatcher.sh
@@ -9,11 +9,21 @@ cd $ROM_TREE/frameworks/av
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/native
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/opt/net/wifi
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Email
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/providers/TelephonyProvider
+git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
+git clean -f -d && git reset --hard
+cd $ROM_TREE/system/media
 git clean -f -d && git reset --hard
 cd $ROM_TREE/vendor/cm
 git clean -f -d && git reset --hard
