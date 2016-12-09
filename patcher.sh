c5983f9529ee4fbea40251a020a2d081268ed213
Sultanxda <sultanxda@gmail.com>
2016-12-08 22:12:51 -0800

    Add December 1, 2016 security patches

--- patcher.sh
+++ patcher.sh
@@ -6,20 +6,31 @@ ROM_TREE=$PWD
 BRANCH=$ROM_TREE/patcher/ZNH5Y
 CUSTOM=$ROM_TREE/patcher/sultan
 
+SECURITY=$ROM_TREE/patcher/ZNH5Y/security
+SECURITY_DEC16=$SECURITY/2016-12-01
+
 APN_LIST_URL=https://raw.githubusercontent.com/CyanogenMod/android_vendor_cm/cm-13.0/prebuilt/common/etc/apns-conf.xml
 
 # Clean up first
 cd $ROM_TREE/build
 git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libavc
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/av
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/ex
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/native
 git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/opt/net/wifi
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/services/Telephony
 git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
@@ -54,3 +65,19 @@ curl -s $APN_LIST_URL > vendor/cm/prebuilt/common/etc/apns-conf.xml
 cd $ROM_TREE/frameworks/base
 git revert --no-edit --no-commit 545dc46798d26328b6dc503663a771b37797eca5
 cd $ROM_TREE
+
+### Security patches
+# 2016-12-01
+patch -d build					-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/build0.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/external-libavc0.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/external-libavc1.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/frameworks-av0.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/frameworks-av1.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/frameworks-base0.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/frameworks-base1.patch
+patch -d frameworks/ex				-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/frameworks-ex0.patch
+patch -d frameworks/opt/net/wifi		-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/frameworks-opt-net-wifi0.patch
+patch -d frameworks/opt/net/wifi		-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/frameworks-opt-net-wifi1.patch
+patch -d packages/services/Telephony		-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/packages-services-Telephony0.patch
+patch -d packages/services/Telephony		-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/packages-services-Telephony1.patch
+patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SECURITY_DEC16/system-core0.patch
--- unpatcher.sh
+++ unpatcher.sh
@@ -5,15 +5,23 @@ ROM_TREE=$PWD
 # Clean up
 cd $ROM_TREE/build
 git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libavc
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/av
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/ex
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/native
 git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/opt/net/wifi
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/services/Telephony
 git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
