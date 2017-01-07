c13ec9ed7ece2c5d5367d4e9f1e709bf572abc99
Sultanxda <sultanxda@gmail.com>
2017-01-06 22:28:28 -0800

    Add January 1, 2017 security patches

--- patcher.sh
+++ patcher.sh
@@ -7,18 +7,43 @@ PATCHER_PATH=$ROM_TREE/patcher
 BRANCH=$PATCHER_PATH/ZNH5Y
 CUSTOM=$PATCHER_PATH/sultan
 
+SECURITY=$ROM_TREE/patcher/ZNH5Y/security
+SECURITY_JAN17=$SECURITY/2017-01-01
+
 APN_LIST_URL=https://raw.githubusercontent.com/CyanogenMod/android_vendor_cm/cm-13.0/prebuilt/common/etc/apns-conf.xml
 
 # Clean up first
 cd $ROM_TREE/build
 git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libavc
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libhevc
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libnl
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libopus
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libvpx
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/tremolo
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/av
 git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/ex
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/ex
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/native
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/opt/net/wifi
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/services/Telephony
 git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
@@ -52,3 +77,22 @@ patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $CUSTOM/system-core2
 # Get APN list from nightly branch and apply patch on top
 curl -s $APN_LIST_URL > vendor/cm/prebuilt/common/etc/apns-conf.xml
 patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $CUSTOM/vendor-cm0.patch
+
+### Security patches
+# 2017-01-01
+patch -d build					-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/build0.patch
+patch -d external/libhevc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/external-libhevc0.patch
+patch -d external/libnl				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/external-libnl0.patch
+patch -d external/libopus			-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/external-libopus0.patch
+patch -d external/libvpx			-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/external-libvpx0.patch
+patch -d external/tremolo			-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/external-tremolo0.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-av0.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-av1.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-av2.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-av3.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-av4.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-av5.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-base0.patch
+patch -d frameworks/ex				-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-ex0.patch
+patch -d frameworks/native			-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/frameworks-native0.patch
+patch -d packages/services/Telephony		-p1 -s -N --no-backup-if-mismatch < $SECURITY_JAN17/packages-services-Telephony0.patch
--- unpatcher.sh
+++ unpatcher.sh
@@ -5,13 +5,35 @@ ROM_TREE=$PWD
 # Clean up
 cd $ROM_TREE/build
 git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libavc
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libhevc
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libnl
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libopus
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libvpx
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/tremolo
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/av
 git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/ex
+git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/ex
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/native
+git clean -f -d && git reset --hard
+cd $ROM_TREE/frameworks/opt/net/wifi
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/services/Telephony
 git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
