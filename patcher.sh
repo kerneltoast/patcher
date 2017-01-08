45875dd612e38a52d4dceb44272d866ba70ac992
Sultanxda <sultanxda@gmail.com>
2017-01-07 17:34:47 -0800

    Add patch to remove CM Stats feature

--- patcher.sh
+++ patcher.sh
@@ -74,6 +74,7 @@ patch -d packages/apps/LockClock		-p1 -s -N --no-backup-if-mismatch < $CUSTOM/pa
 patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Settings0.patch
 patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Settings1.patch
 patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Settings2.patch
+patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Settings3.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $CUSTOM/system-core0.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $CUSTOM/system-core1.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $CUSTOM/system-core2.patch
