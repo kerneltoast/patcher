980d62fb52e11bebd712ca9d85974e9832aa6279
Sultanxda <sultanxda@gmail.com>
2017-03-18 12:09:24 -0700

    Add March 1, 2017 security patches

--- patcher.sh
+++ patcher.sh
@@ -10,17 +10,24 @@ CUSTOM=$PATCHER_PATH/sultan
 SECURITY=$ROM_TREE/patcher/ZNH5Y/security
 SECURITY_JAN17=$SECURITY/2017-01-01
 SECURITY_FEB17=$SECURITY/2017-02-01
+SECURITY_MAR17=$SECURITY/2017-03-01
 
 APN_LIST_URL=https://raw.githubusercontent.com/CyanogenMod/android_vendor_cm/cm-13.0/prebuilt/common/etc/apns-conf.xml
 
 # Clean up first
 cd $ROM_TREE/bionic
 git clean -f -d && git reset --hard
+cd $ROM_TREE/bootable/recovery
+git clean -f -d && git reset --hard
 cd $ROM_TREE/build
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/boringssl
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libavc
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libhevc
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libnfc-nci
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libnl
 git clean -f -d && git reset --hard
@@ -31,8 +38,6 @@ git clean -f -d && git reset --hard
 cd $ROM_TREE/external/tremolo
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/av
-git clean -f -d && git reset --hard
-cd $ROM_TREE/frameworks/ex
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
@@ -46,9 +51,13 @@ cd $ROM_TREE/hardware/libhardware
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Bluetooth
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/CertInstaller
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Messaging
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/PackageInstaller
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
 git clean -f -d && git reset --hard
@@ -129,3 +138,38 @@ patch -d packages/apps/Messaging		-p1 -s -N --no-backup-if-mismatch < $SECURITY_
 patch -d packages/apps/Messaging		-p1 -s -N --no-backup-if-mismatch < $SECURITY_FEB17/packages-apps-Messaging2.patch
 patch -d packages/apps/UnifiedEmail		-p1 -s -N --no-backup-if-mismatch < $SECURITY_FEB17/packages-apps-UnifiedEmail0.patch
 patch -d vendor/cmsdk				-p1 -s -N --no-backup-if-mismatch < $SECURITY_FEB17/vendor-cmsdk0.patch
+
+# 2017-03-01
+patch -d bootable/recovery			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/bootable-recovery0.patch
+patch -d build					-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/build0.patch
+patch -d external/boringssl			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-boringssl0.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc0.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc1.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc2.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc3.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc4.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc5.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc6.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc7.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc8.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc9.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc10.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc11.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc12.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libavc13.patch
+patch -d external/libhevc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libhevc0.patch
+patch -d external/libnfc-nci			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/external-libnfc-nci0.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-av0.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-av1.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-av2.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-av3.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-base0.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-base1.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-base2.patch
+patch -d frameworks/ex				-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-ex0.patch
+patch -d frameworks/opt/net/wifi		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/frameworks-opt-net-wifi0.patch
+patch -d packages/apps/CertInstaller		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-CertInstaller0.patch
+patch -d packages/apps/CertInstaller		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-CertInstaller1.patch
+patch -d packages/apps/Messaging		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-Messaging0.patch
+patch -d packages/apps/Messaging		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-Messaging1.patch
+patch -d packages/apps/PackageInstaller		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-PackageInstaller0.patch
--- unpatcher.sh
+++ unpatcher.sh
@@ -5,11 +5,17 @@ ROM_TREE=$PWD
 # Clean up
 cd $ROM_TREE/bionic
 git clean -f -d && git reset --hard
+cd $ROM_TREE/bootable/recovery
+git clean -f -d && git reset --hard
 cd $ROM_TREE/build
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/boringssl
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libavc
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libhevc
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libnfc-nci
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libnl
 git clean -f -d && git reset --hard
@@ -20,8 +26,6 @@ git clean -f -d && git reset --hard
 cd $ROM_TREE/external/tremolo
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/av
-git clean -f -d && git reset --hard
-cd $ROM_TREE/frameworks/ex
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
@@ -35,9 +39,13 @@ cd $ROM_TREE/hardware/libhardware
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Bluetooth
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/CertInstaller
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Messaging
+git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/PackageInstaller
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
 git clean -f -d && git reset --hard
