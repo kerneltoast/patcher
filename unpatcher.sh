3b37f4abddb9a1ebb84763d248eb3882a7a0210e
Sultanxda <sultanxda@gmail.com>
2017-04-11 00:58:25 -0700

    Add patch to make the cm-13.0 Gallery2 app compile in cm-14.1

--- unpatcher.sh
+++ unpatcher.sh
@@ -11,6 +11,8 @@ cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/native
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Gallery2
+git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
