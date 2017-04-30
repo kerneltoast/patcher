8fcbd83492b5fd35f5a82fa8b90b86dce4b0d0e4
Sultanxda <sultanxda@gmail.com>
2017-04-30 15:17:10 -0700

    Add April 1, 2017 security patches

--- patcher.sh
+++ patcher.sh
@@ -11,6 +11,7 @@ SECURITY=$ROM_TREE/patcher/ZNH5Y/security
 SECURITY_JAN17=$SECURITY/2017-01-01
 SECURITY_FEB17=$SECURITY/2017-02-01
 SECURITY_MAR17=$SECURITY/2017-03-01
+SECURITY_APR17=$SECURITY/2017-04-01
 
 APN_LIST_URL=https://raw.githubusercontent.com/CyanogenMod/android_vendor_cm/cm-13.0/prebuilt/common/etc/apns-conf.xml
 
@@ -27,6 +28,8 @@ cd $ROM_TREE/external/libavc
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libhevc
 git clean -f -d && git reset --hard
+cd $ROM_TREE/external/libmpeg2
+git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libnfc-nci
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libnl
@@ -34,6 +37,10 @@ git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libopus
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/libvpx
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/skia
+git clean -f -d && git reset --hard
+cd $ROM_TREE/external/sonivox
 git clean -f -d && git reset --hard
 cd $ROM_TREE/external/tremolo
 git clean -f -d && git reset --hard
@@ -171,3 +178,28 @@ patch -d packages/apps/CertInstaller		-p1 -s -N --no-backup-if-mismatch < $SECUR
 patch -d packages/apps/Messaging		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-Messaging0.patch
 patch -d packages/apps/Messaging		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-Messaging1.patch
 patch -d packages/apps/PackageInstaller		-p1 -s -N --no-backup-if-mismatch < $SECURITY_MAR17/packages-apps-PackageInstaller0.patch
+
+# 2017-04-01
+patch -d build					-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/build0.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc0.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc1.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc2.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc3.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc4.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc5.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc6.patch
+patch -d external/libavc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libavc7.patch
+patch -d external/libhevc			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libhevc0.patch
+patch -d external/libmpeg2			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libmpeg20.patch
+patch -d external/libmpeg2			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libmpeg21.patch
+patch -d external/libnl				-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-libnl0.patch
+patch -d external/skia				-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-skia0.patch
+patch -d external/sonivox			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/external-sonivox0.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/frameworks-av0.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/frameworks-av1.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/frameworks-av2.patch
+patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/frameworks-av3.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/frameworks-base0.patch
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/frameworks-base1.patch
+patch -d frameworks/native			-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/frameworks-native0.patch
+patch -d packages/services/Telephony		-p1 -s -N --no-backup-if-mismatch < $SECURITY_APR17/packages-services-Telephony0.patch
