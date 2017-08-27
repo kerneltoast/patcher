084936f8eaec7957099579eece881c9ff11ed31b
Sultanxda <sultanxda@gmail.com>
2017-08-26 17:39:31 -0700

    Add patches to retain CMUpdater
    Keep CMUpdater for now until I get around to hacking up the new updater.

--- patcher.sh
+++ patcher.sh
@@ -24,6 +24,8 @@ cd $ROM_TREE/packages/apps/Gallery2
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
+cd $ROM_TREE/packages/apps/Settings
+git clean -f -d && git reset --hard
 cd $ROM_TREE/system/core
 git clean -f -d && git reset --hard
 cd $ROM_TREE/vendor/cm
@@ -44,10 +46,12 @@ patch -d packages/apps/Dialer			-p1 -s -N --no-backup-if-mismatch < $SULTAN/pack
 patch -d packages/apps/Eleven			-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-Eleven0.patch
 patch -d packages/apps/Eleven			-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-Eleven1.patch
 patch -d packages/apps/LockClock		-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-LockClock0.patch
+patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-Settings0.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SULTAN/system-core0.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SULTAN/system-core1.patch
 patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm0.patch
 patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm1.patch
+patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm2.patch
 
 ### Custom patches
 patch -d packages/apps/Gallery2			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Gallery20.patch
